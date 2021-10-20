0fcb90461d7332182e62167f4c77e7ca62519e3d
Jeremy Myers <inernestmusic@gmail.com>
2021-10-20 15:03:55 -0400

    tweaked the DOM tree structure of ItemDetails to move the AddToCart component around as a grid item. then adjusted the grid-template-areas in the media queries to improve responsiveness.

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -45,36 +45,37 @@ function ItemDetails({
   };
 
   return (
-    <div className="content-container" id="item-details-container">
-      <div id="item-details-info-container">
-        <div id="item-details-info">
-          <h3 id="object-details-header">Details</h3>
-          <h4>
-            <strong>Title</strong>: {item.title}
-          </h4>
-          <h4>
-            <strong>Date</strong>: {item.objectDate}
-          </h4>
-          <h4>
-            <strong>Artist</strong>:{" "}
-            {item.artistDisplayName ? item.artistDisplayName : `unknown`}
-          </h4>
-          <h4>
-            <strong>Medium</strong>: {item.medium}
-          </h4>
-          <h4>
-            <strong>Dimensions</strong>: {item.dimensions}
-          </h4>
-          <a
-            className="linkToMet"
-            href={item.objectURL}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Click here to learn more about this piece.
-          </a>
-        </div>
-        <div id="add-item-container">
+    <div id="item-details-container" className="content-container">
+      <div id="item-details-container-grid">
+        <div id="item-details-info-container">
+          <div id="item-details-info">
+            <h3 id="object-details-header">Details</h3>
+            <h4>
+              <strong>Title</strong>: {item.title}
+            </h4>
+            <h4>
+              <strong>Date</strong>: {item.objectDate}
+            </h4>
+            <h4>
+              <strong>Artist</strong>:{" "}
+              {item.artistDisplayName ? item.artistDisplayName : `unknown`}
+            </h4>
+            <h4>
+              <strong>Medium</strong>: {item.medium}
+            </h4>
+            <h4>
+              <strong>Dimensions</strong>: {item.dimensions}
+            </h4>
+            <a
+              className="linkToMet"
+              href={item.objectURL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Click here to learn more about this piece.
+            </a>
+          </div>
+          {/* <div id="add-item-container">
           <AddToCart
             price={price}
             itemToAdd={item}
@@ -93,14 +94,35 @@ function ItemDetails({
           >
             <p> &#8592; back</p>
           </Link>
+        </div> */}
         </div>
-      </div>
-      <div id="item-details-img-container">
-        <img
-          id="item-details-img"
-          src={item.primaryImageSmall}
-          alt={item.title}
-        ></img>
+        <div id="item-details-img-container">
+          <img
+            id="item-details-img"
+            src={item.primaryImageSmall}
+            alt={item.title}
+          ></img>
+        </div>
+        <div id="add-item-container">
+          <AddToCart
+            price={price}
+            itemToAdd={item}
+            itemsInCart={itemsInCart}
+            setItemsInCart={setItemsInCart}
+            selectedQuantity={selectedQuantity}
+            setSelectedQuantity={setSelectedQuantity}
+            currentPath={currentPath}
+          ></AddToCart>
+          <div id="back-container">
+            <Link
+              onClick={handlePathname}
+              id="back-to-shop"
+              to="/shopping-cart/shop"
+            >
+              <p> &#8592; back</p>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
